247eb57798944d450fb1e197c39ff1e3e3c92df0
Trung Nguyen <43191329+kylenguyen1014@users.noreply.github.com>
2019-10-03 22:31:38 -0400

    add route /image handling API merge request

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+
 const fpp = require('face-plusplus-node');
+
 const APIkey = '1ltOS1IXZ1VTu_UyqY3S0HbV_DK3EJwA';
 const APISecret = '2h4nPYp5Hs8qUAsfOM3H8TzQTKuoMZKZ';
 
@@ -20,29 +22,73 @@ app.get('/', (req,res) => {
     res.send("this is working");
 })
 
-app.post('/', (req,res) => {
-    const {template } = req.body;
-    const parameters = {
-        image_base64: template
-    }
-
-    fpp.post('/detect', parameters, (err,response) => {
-        res.send(response);
-    })
+app.post('/image', (req,res) => {
+    const {template, merging } = req.body;
     // console.log(template);
-    // axios.post('https://api-us.faceplusplus.com/facepp/v3/detect', {
-    //         // headers: {
-    //         //     api_key: APIkey,
-    //         //     api_secret: APISecret,
-    //         // },
-    //         image_base64: template, 
-    //         // image_url : 'https://cnet3.cbsistatic.com/img/gk7d6AQXuqmtPNmnZI2gMaNySyA=/970x0/2018/09/05/7274da05-85a9-4646-b41f-a4b22c597507/captain-marvel-brie-larson-1.jpg'
+    // console.log(merging);
+    // const parameter = {
+    //     // image_base64: template
+    //     image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1GcaPGQyXCKT8k4F8LMdDaykGJ54toDWyN6yeoyO4zR4epD76'
+    // }
+    const params = {
+        api_key: APIkey,
+        api_secret: APISecret,
+        // template_base64: template,
+        template_url:template,
+        // template_rectangle: template_rectangle,
+        // merge_base64 : merging
+        merge_url : merging
+    };
+    axios({
+        method : 'post',
+        url :'https://us.faceplusplus.com/imagepp/v1/mergeface',
+        // headers: {'Content-type': 'application/json'},
+        params : params
+    })
+    .then(response1 => {
+        // const data = 'data:image/jpeg;base64,' + response1.data.result;
+        const data =  response1.data;
+        res.send(data);
+    })
+    .catch(err1 => console.log(err1))
+
+    // fpp.post('/detect', parameter, (err,response) => {
+    //     // console.log(response);
+    //     // res.send(response);
+    //     const {top, left, width, height} = response.faces[0].face_rectangle;
+    //     const template_rectangle = `${top},${left},${width},${height}`;
+    //     console.log(template_rectangle);
+    //     const params = {
+    //         api_key: APIkey,
+    //         api_secret: APISecret,
+    //         // template_base64: template,
+    //         template_url: template,
+    //         // template_url: 'https://goodmenproject.com/wp-content/uploads/2019/09/shutterstock_395314813.jpg',
+    //         // template_rectangle: template_rectangle,
+    //         // merge_base64 : merging
+    //         merge_url : merging
+    //         // merge_url :'https://mymodernmet.com/wp/wp-content/uploads/archive/9YPBjDyXBmK6zd25PAM1_gesichtermix14.jpg'
+    //     };
+    //     // this.fpp.mergeFace(merge, (success) => console.log(success), (error) => console.log(error));
+    //     axios({
+    //         method : 'post',
+    //         url :'https://us.faceplusplus.com/imagepp/v1/mergeface',
+    //         // headers: {'Content-type': 'application/json'},
+    //         params : params
     //     })
-    //     .then((response) => {
-    //         res.json(response);
-    //     }, (error) => {
-    //         console.log(error);
+    //     .then(response1 => {
+    //         // const data = 'data:image/jpeg;base64,' + response1.data.result;
+    //         const data =  response1.data;
+            
+    //         // const data = response1.json();
+    //         // res.json(data);
+    //         // console.log(data);
+    //         res.send(data);
     //     })
+    //     .catch(err1 => console.log(err1))
+    // })
+        
+    
 })
 
 /*
