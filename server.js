c93ae413faa24381bd31134da8b1743c8057a0cc
Kyle <kyle@DESKTOP-9ER3K7G.localdomain>
2019-11-14 14:57:00 -0500

    updated package.json

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const uuid = require('uuid/v4');
 const knex = require('knex');
 
 const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : 'Kyle',
-      password : '2bdangyeuroi',
-      database : 'face-swap'
+      user : 'postgres',
+      password : 'password',
+      database : 'face-swap-api'
     }
 });
 db.select('*').from('users').then(data => {
     console.log(data);
+});
 
-
-
-
+// console.log(db.select('*').from('users'));
 
 const app = express();
-// app.use(bodyParser.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
@@ -39,8 +37,15 @@ app.post('/signin', (req,res) => {
 
 })
 
-app.post('register', (req,res) => {
-
+app.post('/register', (req,res) => {
+    const { name, email, password } = req.body;
+    db('users').insert({
+        name : name,
+        email: email,
+        entries: 0,
+        joined: new Date()
+    }).then(console.log);
+    res.send("register");
 })
 /*
 / --> res 
